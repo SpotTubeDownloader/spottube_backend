45d026cea8dbe30213e9d003f3a3b3f4217b878a
santal1210 <sant.nino02@gmail.com>
2024-05-21 19:32:04 +0200

    fix searchName

--- Routes/user_endpoint.js
+++ Routes/user_endpoint.js
@@ -3,7 +3,7 @@ const userRouter = express.Router()
 const database = require('../Database/database');
 const { searchSong, downloadSong } = require('../utils/youtube');
 
-userRouter.post('/saerchName', (req, res) => {
+userRouter.post('/searchName', (req, res) => {
     const songName = req.body.songName + " original song";
     searchSong(songName).then((songs) => {
         res.send(songs);

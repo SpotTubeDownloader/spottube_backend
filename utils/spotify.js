3df4789d804c7d34427171bae8b0a60f8eaae1e2
santal1210 <sant.nino02@gmail.com>
2024-06-04 00:36:36 +0200

    add spotify download

---
+++ utils/spotify.js
@@ -0,0 +1,74 @@
+const axios = require ("axios");
+require('dotenv').config();
+
+
+
+//Spotify API
+const clientId =  process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+async function getAccessToken() {
+    try {
+        const response = await axios.post('https://accounts.spotify.com/api/token', null, {
+            params: {
+                grant_type: 'client_credentials'
+            },
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+            },
+        });
+        const accessToken = response.data.access_token;
+        return accessToken;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+
+//function to get the song name frome the spotify's link
+async function getSongName(link) {
+    try{
+        let songId = link.split('/')[4];
+        songId = songId.split('?')[0];
+        console.log("[INFO]: SongID ",songId);
+        const accessToken = await getAccessToken();
+        console.log("[INFO]: Access Token ",accessToken);   
+        const response = await axios.get(`https://api.spotify.com/v1/tracks/${songId}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
+        const songName = response.data.name;
+        console.log(songName);
+        return songName;
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+async function getSongArtist(link) {
+    try {
+        const accessToken = await getAccessToken();
+        let songId = link.split('/')[4];
+        songId = songId.split('?')[0];
+
+        const response = await axios.get(`https://api.spotify.com/v1/tracks/${songId}`, {
+            headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        const artistName = response.data.artists[0].name;
+        console.log(artistName);
+        return artistName;   
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = { getSongName, getSongArtist };
+
+
